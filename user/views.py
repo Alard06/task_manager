4d9d524de8080af11145e3e64f0ab9391025f6db
Alard06 <NikitaMineev2020@yandex.ru>
2023-08-23 19:18:22 +0800

    change profile v3

--- user/views.py
+++ user/views.py
@@ -7,7 +7,7 @@ from django.views.generic import DetailView, ListView, CreateView
 from django.db.models import Q
 from django.views.generic.edit import FormMixin
 
-from task.models import Profile
+from task.models import Profile, Task
 from user.forms import RegisterUserForm, UpdateCheckEmail
 
 
@@ -18,25 +18,29 @@ class ProfileView(FormMixin, DetailView):
 
     def get_success_url(self):
         return reverse('profile_view', kwargs={'pk': self.object.pk})
+
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
         form = self.get_form()
         if form.is_valid():
-            check_email = form.cleaned_data['check_email']
-            p = Profile.objects.get(pk=request.user.pk)
-            p.check_email = check_email
-            p.save()
-            print(p.check_email)
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
 
     def form_valid(self, form):
+        p = Profile.objects.get(pk=self.request.user.pk)
+        p.check_email = form.cleaned_data['check_email']
+        p.save()
+        print(f'{p.user.username}|{p.check_email}')
         return super().form_valid(form)
 
     def form_invalid(self, form):
         return super().form_invalid(form)
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['tasks'] = Task.objects.filter(owner=self.request.user.pk)
+        return context
 
 class ProfileListView(ListView):
     model = Profile
